63038ff642e0b89d83f827bf209d4a134440f2c9
Roman Lavrik <lavrikrom2@gmail.com>
2022-06-21 02:22:44 +0300

    fix: timeout

--- scripts/index.js
+++ scripts/index.js
@@ -15,10 +15,10 @@ document.querySelector('.videocard-first__video').addEventListener('loadeddata',
   const title = document.querySelector('.preload__title');
   title.style.display = 'none';
   preload.style.width = '0px';
-  this.setTimeout(() => {
+  setTimeout(() => {
     preload.style.display = 'none';
   }, 1400);
-})
+});
 
 const smoothScrollTrigger = (containerId, videoClass) => {
   const video = document.querySelector(videoClass);
